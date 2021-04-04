b93b854b6d29a45ffe4821045e1bf441d2d238ba
Nurlan <desigantoshka@gmail.com>
2021-04-05 00:37:48 +0600

    import и исправление файла

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import index from './reducers';
+import rootReducer from './reducers';
 
-const store = createStore(index, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
